ea90adae5feb752e342d42aa1f1423699343c65b
ArtikDemonik <61469976+ArtikDemonik@users.noreply.github.com>
2021-11-09 19:13:30 +0300

    ой, забыл
    вспомнил

---
+++ coding.py
@@ -0,0 +1,9 @@
+import random
+def coding(text):
+    a = 0
+    b = random.randint(100,1000)
+    for i in name:
+        a+=ord(i)+b*2
+        b+=ord(i)*random.randint(1,200)
+        a=round((a/228)*(a/100000))
+    return a
